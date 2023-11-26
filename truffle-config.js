f076ce0123626e17d56ba44365e6609d172529bd
sreejaatluri03 <atluri.sreeja@triconinfotech.com>
2023-11-26 05:49:57 -0700

    feat: UI for vote cast and registration

---
+++ truffle-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  compilers: {
+    solc: {
+      version: '0.5.15',
+    },
+  },
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*', // Match any network id
+    },
+  },
+};
